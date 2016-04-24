1c54bb6012a7c6fd329f3d6296e43157f7078342
Philipp Hagemeister <phihag@phihag.de>
2016-04-24 03:15:21 +0200

    add BWF option 2 tests

--- js/i18n_en.js
+++ js/i18n_en.js
@@ -160,6 +160,7 @@ var i18n_en = {
 'settings:counting:3x21': 'BWF (3x21)',
 'settings:counting:2x21+11': 'German Bundesliga 2016/2017 (2x21, 3rd game until 11)',
 'settings:counting:5x11_15': 'BWF 2016 Experiment Option 1 (5x11 until max. 15)',
+'settings:counting:5x11/3': 'BWF 2016 Experiment Option 2 (5x11 with 3 point challenge)',
 
 'settings:Settings': 'Settings',
 'settings:Court Description': 'Court:',
