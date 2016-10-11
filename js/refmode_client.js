c6af72313b1a61e48ca4fee5133720cf32b9d74b
Philipp Hagemeister <phihag@phihag.de>
2016-10-11 22:49:13 +0200

    move refmode connection creation into its own file

--- js/refmode_client.js
+++ js/refmode_client.js
@@ -1,77 +1,24 @@
 'use strict';
 
 var refmode_client = (function() {
-var enabled = false;
-var ws;
-var ws_url;
-
-function status_str(s) {
-	if (!enabled) {
-		return s._('refmode:status:disabled');
-	}
-	if (ws.bup_connected) {
-		return s._('refmode:status:connected to hub');
-	}
-	return s._('refmode:status:enabled');
-}
+var rc = refmode_conn(handle_change, handle_msg);
 
 function update_status_str(s) {
-	uiu.text_qs('.refmode_status', status_str(s));
+	uiu.text_qs('.refmode_status', rc.status_str(s));
+}
+
+function handle_msg() {
+	console.log('got message', arguments); // eslint-disable-line no-console
+}
+
+function handle_change(estate) {
+	network.errstate('refmode.client.ws', estate);
+	update_status_str(state);
 }
 
 function on_settings_change(s) {
-	var settings_enabled = s.settings.refmode_client_enabled;
-	var changed = false;
-
-	if (settings_enabled !== enabled) {
-		enabled = settings_enabled;
-		changed = true;
-	}
-
-	var new_ws_url = s.settings.refmode_client_ws_url;
-	if (new_ws_url !== ws_url) {
-		ws_url = new_ws_url;
-		changed = true;
-	}
-
-	if (changed) {
-		disconnect();
-		if (enabled) {
-			connect(ws_url, function(estate) {
-				network.errstate('refmode.client.ws', estate);
-				update_status_str(s);
-			});
-		}
-	}
-
+	rc.on_settings_change(s.settings.refmode_client_enabled, s.settings.refmode_client_ws_url);
 	update_status_str(s);
-}
-
-function connect(ws_url, status_cb) {
-	var my_ws = new WebSocket(ws_url, 'bup-refmode');
-	ws = my_ws;
-	my_ws.onopen = function() {
-		my_ws.bup_connected = true;
-		status_cb(null);
-	};
-	my_ws.onmessage = function() {
-		console.log('got message', arguments); // eslint-disable-line no-console
-	};
-	my_ws.onclose = function() {
-		if (my_ws.bup_die) {
-			return;
-		}
-		status_cb(state._('refmode:lost connection'));
-		connect(ws_url, status_cb);
-	};
-}
-
-function disconnect() {
-	if (ws) {
-		ws.bup_die = true;
-		ws.close();
-		ws = null;
-	}
 }
 
 function ui_init(s) {
@@ -89,6 +36,7 @@ return {
 if ((typeof module !== 'undefined') && (typeof require !== 'undefined')) {
 	var network = require('./network');
 	var uiu = require('./uiu');
+	var refmode_conn = require('./refmode_conn');
 
 	module.exports = refmode_client;
 }
---
+++ js/refmode_conn.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var refmode_conn = (function(status_cb, handle_msg) {
+var ws;
+var enabled = false;
+var ws_url;
+
+function connect(ws_url) {
+	var my_ws = new WebSocket(ws_url, 'bup-refmode');
+	ws = my_ws;
+	my_ws.onopen = function() {
+		my_ws.bup_connected = true;
+		status_cb(null);
+	};
+	my_ws.onmessage = handle_msg;
+	my_ws.onclose = function() {
+		if (my_ws.bup_die) {
+			return;
+		}
+		status_cb(state._('refmode:lost connection'));
+		connect(ws_url, status_cb);
+	};
+}
+
+function disconnect() {
+	if (ws) {
+		ws.bup_die = true;
+		ws.close();
+		ws = null;
+	}
+}
+
+function on_settings_change(new_enabled, new_ws_url) {
+	var changed = false;
+
+	if (new_enabled !== enabled) {
+		enabled = new_enabled;
+		changed = true;
+	}
+	if (new_ws_url !== ws_url) {
+		ws_url = new_ws_url;
+		changed = true;
+	}
+
+	if (changed) {
+		disconnect();
+		if (enabled) {
+			connect(ws_url);
+		}
+	}
+}
+
+function status_str(s) {
+	if (!enabled) {
+		return s._('refmode:status:disabled');
+	}
+	if (ws.bup_connected) {
+		return s._('refmode:status:connected to hub');
+	}
+	return s._('refmode:status:enabled');
+}
+
+return {
+	on_settings_change: on_settings_change,
+	status_str: status_str,
+};
+});
+
+/*@DEV*/
+if ((typeof module !== 'undefined') && (typeof require !== 'undefined')) {
+	module.exports = refmode_referee;
+}
+/*/@DEV*/
