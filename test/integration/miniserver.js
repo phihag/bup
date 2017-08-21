b4782e2009260e53c8c90e288f8067d9557ac43c
Philipp Hagemeister <phihag@phihag.de>
2017-08-21 20:34:49 +0200

    add dependencies for integration test

--- test/integration/miniserver.js
+++ test/integration/miniserver.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 'use strict';
 
 const fs = require('fs');
@@ -5,65 +6,35 @@ const http = require('http');
 const path = require('path');
 const url = require('url');
 
-function server() {
+function _err(res, errcode) {
+	res.writeHead(errcode, {'Content-Type': 'text/plain'});
+	res.end('Error ' + errcode);
+}
+
+// Callback arguments: (err, url_base)
+function server(callback) {
 	const ROOT_DIR = path.dirname(path.dirname(__DIR));
-	const serv = http.createServer(function (req, res) {
 
+	const serv = http.createServer((req, res) => {
+		if (! ['GET', 'HEAD'].includes(req.method)) {
+			return _err(res, 405);
+		}
+
+		const parsed_url = url.parse(req.url);
+		console.log(parsed_url);
+		// TODO determine filename
+		// TODO determine MIME type
 	});
-	serv.listen('::1', 0);
-
+	serv.listen(0, '::1', () => {
+		callback(null, 'http://[::1]:' + serv.address().port + '/');
+	});
+	serv.on('error', (err) => {
+		throw err;
+	});
 }
 
 
-  console.log(`${req.method} ${req.url}`);
-
-  // parse URL
-  const parsedUrl = url.parse(req.url);
-  // extract URL path
-  let pathname = `.${parsedUrl.pathname}`;
-  // based on the URL path, extract the file extention. e.g. .js, .doc, ...
-  const ext = path.parse(pathname).ext;
-  // maps file extention to MIME typere
-  const map = {
-    '.ico': 'image/x-icon',
-    '.html': 'text/html',
-    '.js': 'text/javascript',
-    '.json': 'application/json',
-    '.css': 'text/css',
-    '.png': 'image/png',
-    '.jpg': 'image/jpeg',
-    '.wav': 'audio/wav',
-    '.mp3': 'audio/mpeg',
-    '.svg': 'image/svg+xml',
-    '.pdf': 'application/pdf',
-    '.doc': 'application/msword'
-  };
-
-  fs.exists(pathname, function (exist) {
-    if(!exist) {
-      // if the file is not found, return 404
-      res.statusCode = 404;
-      res.end(`File ${pathname} not found!`);
-      return;
-    }
-
-    // if is a directory search for index file matching the extention
-    if (fs.statSync(pathname).isDirectory()) pathname += '/index' + ext;
-
-    // read file from file system
-    fs.readFile(pathname, function(err, data){
-      if(err){
-        res.statusCode = 500;
-        res.end(`Error getting the file: ${err}.`);
-      } else {
-        // if the file is found, set Content-type and send data
-        res.setHeader('Content-type', map[ext] || 'text/plain' );
-        res.end(data);
-      }
-    });
-  });
-
-
-})
-
-console.log(`Server listening on port ${port}`);
+if (require.main === module) {
+	// Run in CLI
+	
+}
