be383cf0587956020c8422b98839f663fc98803c
Philipp Hagemeister <phihag@phihag.de>
2017-10-31 20:22:51 +0100

    doc: explain it is a slideshow

--- doc/slides.js
+++ doc/slides.js
@@ -1,6 +1,14 @@
-Reveal.initialize({
-	transition: 'none',
-	slideNumber: 'c',
-	history: true,
-	center: false,
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+	Reveal.initialize({
+		transition: 'none',
+		slideNumber: 'c',
+		history: true,
+		center: false,
+	});
+
+	document.querySelector('.next-slide').addEventListener('click', function() {
+		Reveal.next();
+	});
 });
