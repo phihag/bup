d2d9fc50e003f9a14ee884f054e0d108c8a03738
Philipp Hagemeister <phihag@phihag.de>
2017-10-31 20:36:40 +0100

    allow clicking anywhere

--- doc/slides.js
+++ doc/slides.js
@@ -11,4 +11,17 @@ document.addEventListener('DOMContentLoaded', function() {
 	document.querySelector('.next-slide').addEventListener('click', function() {
 		Reveal.next();
 	});
+
+	document.querySelector('body').addEventListener('click', function(e) {
+		var el = e.target;
+		do {
+			console.log(el);
+			var tagName = el.tagName.toLowerCase();
+			if ((tagName === 'a') || (tagName === 'button')) {
+				return;
+			}
+		} while ((el = el.parentNode) && (el.nodeType === 1));
+
+		Reveal.next();
+	});
 });
