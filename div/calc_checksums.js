b221c05f58990c4dbba26c6e060e92443b36eb49
Philipp Hagemeister <phihag@phihag.de>
2020-03-08 14:02:07 +0100

    Update calc_checksums to use promises
    Previously, it also failed silently

--- div/calc_checksums.js
+++ div/calc_checksums.js
@@ -1,57 +1,58 @@
 #!/usr/bin/env node
 'use strict';
 
+const argparse = require('argparse');
 const fs = require('fs');
-const fs_walk = require('fs-walk');
 const path = require('path');
+const {promisify} = require('util');
 
 const script_utils = require('./script_utils');
 
-
-function main() {
-	const args = process.argv.slice(2);
-	if (args.length !== 3) {
-		console.log('Usage: calc_checksums.js DISTDIR INCLUDE_PATH OUTFILE'); // eslint-disable-line no-console
-		process.exit(1);
-		return;
-	}
-	const dist_dir = path.resolve(args[0]);
-	const include_path = args[1];
-	const outfile = path.resolve(args[2]);
-
-	const checksums = {};
-	fs_walk.files(path.join(dist_dir, include_path), function(basedir, filename, _, next) {
-		const fn = path.resolve(basedir, filename);
-		const vfn = path.relative(dist_dir, fn);
-
-		if (fn === outfile) {
-			return next();
-		}
-
-		script_utils.hash_file(fn, (err, sha512) => {
-			if (err) return next(err);
-
-			checksums[vfn] = {sha512};
-			next();
-		}, 'sha512');
-	}, function(err) {
-		if (err) {
-			console.log(err);
-			process.exit(2);
-		}
-
-		const fns = Object.keys(checksums);
-		fns.sort();
-		const checksums_json = '{' + fns.map(function(fn) {
-			return JSON.stringify(fn) + ':' + JSON.stringify(checksums[fn]);
-		}).join(',') + '}';
-		fs.writeFile(outfile, checksums_json, {encoding: 'utf8'}, function(err) {
-			if (err) {
-				console.log('Failed to write checksums file', err);
-				process.exit(3);
-			}
-		});
-	});
+async function* walkFiles(dir) {
+    for await (const dirent of await fs.promises.opendir(dir)) {
+        const memberPath = path.resolve(path.join(dir, dirent.name));
+        if (dirent.isFile()) {
+            yield memberPath;
+        } else if (dirent.isDirectory()) {
+            yield* walkFiles(memberPath);
+        }
+    }
 }
 
-main();
+
+async function main() {
+    const parser = new argparse.ArgumentParser({description: 'Generate checksum.json file for bup dist'});
+    parser.addArgument('DISTDIR', {help: 'Root distribution directory'});
+    parser.addArgument('INCLUDE_PATH', {help: 'Path to files to checksum, relative to DISTDIR'});
+    parser.addArgument('OUTFILE', {help: 'File to write'});
+    const args = parser.parseArgs();
+
+    const dist_dir = path.resolve(args.DISTDIR);
+    const include_path = args.INCLUDE_PATH;
+    const outfile = path.resolve(args.OUTFILE);
+
+    const checksums = {};
+    for await (const fn of walkFiles(path.join(dist_dir, include_path))) {
+        if (fn === outfile) continue;
+
+        const vfn = path.relative(dist_dir, fn);
+        const sha512 = await promisify(script_utils.hash_file)(fn);
+        checksums[vfn] = {sha512};
+    }
+
+    const fns = Object.keys(checksums);
+    fns.sort();
+    const checksums_json = '{' + fns.map(function(fn) {
+        return JSON.stringify(fn) + ':' + JSON.stringify(checksums[fn]);
+    }).join(',') + '}';
+    await promisify(fs.writeFile)(outfile, checksums_json, {encoding: 'utf8'});
+}
+
+(async () => {
+    try {
+        await main();
+    } catch (e) {
+        console.error(e.stack);
+        process.exit(2);
+    }
+})();
