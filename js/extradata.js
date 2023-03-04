727f0b3d322a52f4beee8e85a9e10ccce18c21db
Philipp Hagemeister <phihag@phihag.de>
2023-03-04 12:35:55 +0100

    color STC BW Solingen

--- js/extradata.js
+++ js/extradata.js
@@ -16,7 +16,7 @@ var TEAM_COLORS = {
 	'Bischmisheim': {fg: '#0077d3'},
 	'BV Mülheim': {fg: '#ee0000'},
 	'GW Mülheim': {fg: '#56cf6a'},
-	'Solingen': {fg: '#89deff'}, // STC BW Solingen
+	'Solingen': {fg: '#b6bf57'}, // STC BW Solingen
 	'Dortelweil': {fg: '#f55a44'},
 	'EBT Berlin': {fg: '#0d9aff'},
 	'Friedrichsdorf': {bg: '#008131', fg: '#ffffff'},
