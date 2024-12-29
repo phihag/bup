0bc6dfbfedeeba0f20e822e4e3ae5c7a56e577f9
tengmel <tim.englich@t-online.de>
2024-12-29 11:32:37 +0100

    #92: BUP/BTS: Advertisement: Make it possible to confgure the advertisements which should be displayed in BTS

--- js/btsh.js
+++ js/btsh.js
@@ -279,6 +279,17 @@ function btsh(baseurl, tournament_key) {
 			case 'confirm-match-finished':
 				confirm_match_finished();
 				break;
+			case 'advertisement_add':
+				state.dads.push(c.val)
+				break;
+			case 'advertisement_remove':
+				if (state.dads) {
+					const changed_t = utils.find(state.dads, m => m._id === c.val.advertisement_id);
+					if (changed_t) {
+						state.dads.splice(state.dads.indexOf(changed_t), 1);
+					}
+				}
+				break;
 			case 'courts-update':
 
 				var courts = c.val.map(function (rc) {
